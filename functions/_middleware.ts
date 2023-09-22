bbca3bd0d8f16fa0678dc2fac79bacd6cf02d888
Arne Tarara <arne@datafuse.de>
2023-09-22 09:31:15 +0200

    New Services page with email form through Cloudflare (#18)

---
+++ functions/_middleware.ts
@@ -0,0 +1,15 @@
+import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+
+export const onRequest = mailchannelsPlugin({
+  personalizations: [
+    {
+      to: [{ name: "Green Coding Berlin", email: "info@green-coding.berlin" }],
+    },
+  ],
+  from: { name: "Green Coding Berlin Website Enquiry", email: "no-reply@green-coding.berlin" },
+  respondWith: () =>
+    new Response(null, {
+      status: 302,
+      headers: { Location: "/thank-you" },
+    }),
+});
